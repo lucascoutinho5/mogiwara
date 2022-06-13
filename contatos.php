67aed17e72412a2b06969c3787e99547de4479c1
lucas coutinho <lucascoutinho50@hotmail.com>
2022-06-13 19:03:02 -0300

    exercicio seg

---
+++ contatos.php
@@ -0,0 +1,25 @@
+<?php
+
+	/*echo $_POST["nome"];
+	echo "<br>";
+	echo $_POST["email"];
+	echo "<br>";
+	echo $_POST["tipo-contato"];
+	echo $_POST["numero1"]+$_POST["numero2"];*/
+	
+	//echo $_GET["nome"];
+	//echo "<br>";
+	//echo $_GET["email"];
+	//echo "<br>";
+	//echo $_GET["tipo-contato"];
+	//echo $_GET["numero1"]+$_GET["numero2"];
+	
+	
+	
+	/*$nome=$_GET["nome"];
+	$email=$_GET["email"];
+	$status=$_GET["tipo-contato"];
+	$valor=$_GET["numero1"];
+	$b=$_GET["numero2"];*/
+	
+			
